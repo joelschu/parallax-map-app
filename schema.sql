ff46d87931758ad2901fb2cdfaef702077e0002f
joelschu <joelschuelin@gmail.com>
2026-07-18 13:19:14 +0200

    add frontend index and schema sql

--- schema.sql
+++ schema.sql
@@ -5,11 +5,15 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE TABLE IF NOT EXISTS shapes (
   id           SERIAL PRIMARY KEY,
   name         TEXT,
+  area          TEXT,
+  intervention  TEXT,
+  item          TEXT,
   start_date   DATE,
   end_date     DATE,
   cost         NUMERIC,
   funders      TEXT,
   policy       TEXT,
+  description   TEXT,
   geom         GEOMETRY(Geometry, 4326) NOT NULL,
   properties   JSONB NOT NULL DEFAULT '{}'::jsonb,
   created_at   TIMESTAMPTZ NOT NULL DEFAULT now()
